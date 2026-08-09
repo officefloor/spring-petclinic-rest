fcad6a0769beb7700133a43ed5f46f28ff436902
Daniel Sagenschneider <daniel@officefloor.net>
2026-08-09 09:43:05 +0800

    cp07 agent registration-date

--- src/main/java/org/springframework/samples/petclinic/rest/controller/v1/OwnerRestControllerV1.java
+++ src/main/java/org/springframework/samples/petclinic/rest/controller/v1/OwnerRestControllerV1.java
@@ -16,6 +16,7 @@
 
 package org.springframework.samples.petclinic.rest.controller.v1;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 
@@ -118,6 +119,9 @@ public class OwnerRestControllerV1 implements OwnersApi {
             }
         }
         owner.setTelephone(telephone);
+        if (owner.getRegistrationDate() == null) {
+            owner.setRegistrationDate(LocalDate.now());
+        }
         this.clinicService.saveOwner(owner);
         OwnerDto ownerDto = ownerMapper.toOwnerDto(owner);
         headers.setLocation(UriComponentsBuilder.newInstance()
