6a44011bbb46606f8c65656e7b120122d1a5cff9
Daniel Sagenschneider <daniel@officefloor.net>
2026-08-09 12:50:44 +0800

    cp43 agent audit-enriched

--- src/main/java/org/springframework/samples/petclinic/rest/controller/v1/OwnerRestControllerV1.java
+++ src/main/java/org/springframework/samples/petclinic/rest/controller/v1/OwnerRestControllerV1.java
@@ -210,9 +210,9 @@ public class OwnerRestControllerV1 implements OwnersApi {
         this.clinicService.saveOwner(owner);
         OwnerDto ownerDto = ownerMapper.toOwnerDto(owner);
         applyMembership(ownerDto, owner);
-        AUDIT.info("owner created: id={} customerCode={} registrationDate={} membershipLevel={}",
+        AUDIT.info("owner created: id={} customerCode={} registrationDate={} membershipLevel={} membershipNumber={}",
             owner.getId(), owner.getCustomerCode(), owner.getRegistrationDate(),
-            ownerDto.getMembershipLevel());
+            ownerDto.getMembershipLevel(), owner.getMembershipNumber());
         ownerDto.setBulkSignupWarning(isBulkSignupDay());
         headers.setLocation(UriComponentsBuilder.newInstance()
             .path("/api/owners/{id}").buildAndExpand(owner.getId()).toUri());
