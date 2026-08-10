4899dc1d0be6344f7416688e128466afb128e9be
Daniel Sagenschneider <daniel@officefloor.net>
2026-08-11 06:49:56 +0800

    cp21 agent audit-create

--- src/main/java/org/springframework/samples/petclinic/rest/function/owner/RespondWithOwnerCreated.java
+++ src/main/java/org/springframework/samples/petclinic/rest/function/owner/RespondWithOwnerCreated.java
@@ -2,6 +2,9 @@ package org.springframework.samples.petclinic.rest.function.owner;
 
 import java.net.URI;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.officefloor.plugin.variable.Val;
 import net.officefloor.web.ObjectResponse;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +14,13 @@ import org.springframework.samples.petclinic.rest.dto.OwnerDto;
 
 public class RespondWithOwnerCreated {
 
+    private static final Logger AUDIT = LoggerFactory.getLogger("AUDIT");
+
     public void service(@Val Owner owner, OwnerMapper ownerMapper,
             ObjectResponse<ResponseEntity<OwnerDto>> response) {
         OwnerDto dto = ownerMapper.toOwnerDto(owner);
+        AUDIT.info("Owner created: id={} customerCode={} registrationDate={}",
+                owner.getId(), owner.getCustomerCode(), owner.getRegistrationDate());
         response.send(ResponseEntity.created(URI.create("/api/owners/" + owner.getId())).body(dto));
     }
 }
