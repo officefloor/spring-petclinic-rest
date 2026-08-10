62adcc455bf4a2f20d66c1faff36815635db5754
Daniel Sagenschneider <daniel@officefloor.net>
2026-08-10 12:23:30 +0800

    cp07 agent registration-date

--- src/main/resources/db/h2/schema.sql
+++ src/main/resources/db/h2/schema.sql
@@ -35,7 +35,8 @@ CREATE TABLE IF NOT EXISTS owners (
   address VARCHAR(255) NOT NULL,
   city VARCHAR(80) NOT NULL,
   telephone VARCHAR(20) NOT NULL,
-  email VARCHAR(255)
+  email VARCHAR(255),
+  registration_date DATE
 );
 
 CREATE INDEX idx_owners_last_name ON owners(last_name);
