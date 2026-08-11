6c701a8ea3607ac2eddcfeefcbc62df1a37f83a4
Daniel Sagenschneider <daniel@officefloor.net>
2026-08-11 20:51:47 +0800

    cp45 agent salutation

--- src/main/resources/db/postgres/schema.sql
+++ src/main/resources/db/postgres/schema.sql
@@ -31,6 +31,7 @@ CREATE TABLE IF NOT EXISTS owners (
                                       city       TEXT,
                                       telephone  TEXT,
                                       email      TEXT,
+                                      title      TEXT,
                                       registration_date DATE,
                                       birth_date DATE,
                                       customer_code TEXT,
