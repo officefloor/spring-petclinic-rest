6baf21adb174e3d19cae4552f39c196d215082d7
Daniel Sagenschneider <daniel@officefloor.net>
2026-08-15 06:53:16 +0800

    cp45 agent salutation

--- src/main/java/org/springframework/samples/petclinic/mapper/OwnerMapper.java
+++ src/main/java/org/springframework/samples/petclinic/mapper/OwnerMapper.java
@@ -23,6 +23,9 @@ public interface OwnerMapper {
         expression = "java(CustomerCodeCheckDigit.of(owner.getCustomerCode()))")
     @Mapping(target = "displayName",
         expression = "java(owner.getLastName() + \", \" + owner.getFirstName())")
+    @Mapping(target = "salutation",
+        expression = "java(owner.getTitle() == null || owner.getTitle().isBlank() ? owner.getLastName()"
+            + " : owner.getTitle() + \" \" + owner.getLastName())")
     @Mapping(target = "telephoneDisplay",
         expression = "java(TelephoneDisplay.of(owner.getTelephone()))")
     @Mapping(target = "initials",
