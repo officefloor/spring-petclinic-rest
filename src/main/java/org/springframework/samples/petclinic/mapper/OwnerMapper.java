7ecef0928f1c86bf23fd66f8537c670507eea277
Daniel Sagenschneider <daniel@officefloor.net>
2026-08-15 23:35:39 +0800

    cp06 agent initials

--- src/main/java/org/springframework/samples/petclinic/mapper/OwnerMapper.java
+++ src/main/java/org/springframework/samples/petclinic/mapper/OwnerMapper.java
@@ -20,6 +20,9 @@ public interface OwnerMapper {
 
     @Mapping(target = "displayName",
             expression = "java(owner.getLastName() + \", \" + owner.getFirstName())")
+    @Mapping(target = "initials",
+            expression = "java(Character.toUpperCase(owner.getFirstName().charAt(0)) + \".\" "
+                    + "+ Character.toUpperCase(owner.getLastName().charAt(0)) + \".\")")
     OwnerDto toOwnerDto(Owner owner);
 
     Owner toOwner(OwnerDto ownerDto);
