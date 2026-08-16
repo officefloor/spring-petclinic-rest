7b5c335ba21a7869f2b8e0ecce6c6040d0807850
Daniel Sagenschneider <daniel@officefloor.net>
2026-08-16 14:52:39 +0800

    cp17 agent locality

--- src/main/java/org/springframework/samples/petclinic/mapper/OwnerMapper.java
+++ src/main/java/org/springframework/samples/petclinic/mapper/OwnerMapper.java
@@ -24,6 +24,8 @@ public interface OwnerMapper {
         expression = "java(Character.toUpperCase(owner.getFirstName().charAt(0)) + \".\" + Character.toUpperCase(owner.getLastName().charAt(0)) + \".\")")
     @Mapping(target = "membershipTier",
         expression = "java(java.lang.Integer.valueOf(0).equals(owner.getNamesakeCount()) && owner.getEmail() != null && !owner.getEmail().isBlank() ? \"SILVER\" : \"BRONZE\")")
+    @Mapping(target = "locality",
+        expression = "java(\"Sydney\".equals(owner.getCity()) ? \"NSW\" : \"Melbourne\".equals(owner.getCity()) ? \"VIC\" : \"Brisbane\".equals(owner.getCity()) ? \"QLD\" : \"UNKNOWN\")")
     OwnerDto toOwnerDto(Owner owner);
 
     Owner toOwner(OwnerDto ownerDto);
