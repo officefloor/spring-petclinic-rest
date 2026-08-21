6e39bec8465637c1ecf2be0fd9c71c36a302f112
Daniel Sagenschneider <daniel@officefloor.net>
2026-08-22 05:33:56 +0800

    cp14 agent membership-number

--- src/main/java/org/springframework/samples/petclinic/mapper/OwnerMapper.java
+++ src/main/java/org/springframework/samples/petclinic/mapper/OwnerMapper.java
@@ -22,6 +22,8 @@ public interface OwnerMapper {
             expression = "java(owner.getLastName() + \", \" + owner.getFirstName())")
     @Mapping(target = "initials",
             expression = "java(owner.getFirstName().substring(0, 1).toUpperCase() + \".\" + owner.getLastName().substring(0, 1).toUpperCase() + \".\")")
+    @Mapping(target = "membershipNumber",
+            expression = "java((owner.getCustomerCode() == null || owner.getRegistrationDate() == null) ? null : owner.getCustomerCode() + \"-M\" + String.format(\"%02d\", owner.getRegistrationDate().getYear() % 100))")
     OwnerDto toOwnerDto(Owner owner);
 
     Owner toOwner(OwnerDto ownerDto);
