f59bf6db536e8833358cffc2c7a3d33664fab8d0
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-01 02:22:28 +0800

    cp21 agent audit-create

--- src/main/java/org/springframework/samples/petclinic/model/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/model/Owner.java
@@ -36,6 +36,7 @@ import java.util.*;
  */
 @Entity
 @Table(name = "owners")
+@EntityListeners(OwnerAuditListener.class)
 public class Owner extends Person {
     @Column(name = "address")
     @NotEmpty
---
+++ src/main/java/org/springframework/samples/petclinic/model/OwnerAuditListener.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.samples.petclinic.model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jakarta.persistence.PostPersist;
+
+/**
+ * Emits an audit trail entry when an {@link Owner} is created. On a successful
+ * insert the {@code AUDIT} logger records the new owner's id, customer code and
+ * registration date.
+ */
+public class OwnerAuditListener {
+
+    private static final Logger AUDIT = LoggerFactory.getLogger("AUDIT");
+
+    @PostPersist
+    void onCreate(Owner owner) {
+        AUDIT.info("owner created id={} customerCode={} registrationDate={}",
+            owner.getId(), owner.getCustomerCode(), owner.getRegistrationDate());
+    }
+
+}
