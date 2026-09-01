65b9eebf3d28ef7b9b3973034129f82854d9bb35
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-01 09:47:50 +0800

    cp43 agent audit-enriched

--- src/main/java/org/springframework/samples/petclinic/rest/function/owner/AuditOwnerCreated.java
+++ src/main/java/org/springframework/samples/petclinic/rest/function/owner/AuditOwnerCreated.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.officefloor.plugin.variable.Val;
+import org.springframework.samples.petclinic.mapper.OwnerMapper;
 import org.springframework.samples.petclinic.model.Owner;
 
 /**
@@ -15,8 +16,9 @@ public class AuditOwnerCreated {
 
     private static final Logger AUDIT = LoggerFactory.getLogger("AUDIT");
 
-    public void service(@Val Owner owner) {
-        AUDIT.info("owner created id={} customerCode={} registrationDate={}",
-                owner.getId(), owner.getCustomerCode(), owner.getRegistrationDate());
+    public void service(@Val Owner owner, OwnerMapper mapper) {
+        AUDIT.info("owner created id={} customerCode={} registrationDate={} membershipLevel={} membershipNumber={}",
+                owner.getId(), owner.getCustomerCode(), owner.getRegistrationDate(),
+                mapper.membershipLevel(owner), mapper.membershipNumber(owner));
     }
 }
