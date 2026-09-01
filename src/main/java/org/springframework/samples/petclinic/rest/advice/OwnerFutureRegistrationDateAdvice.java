326b56a9f2ae02c662633da7ab1c1fb0848a8772
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-01 14:44:44 +0800

    cp33 agent no-future-date

---
+++ src/main/java/org/springframework/samples/petclinic/rest/advice/OwnerFutureRegistrationDateAdvice.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.samples.petclinic.rest.advice;
+
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.samples.petclinic.rest.dto.OwnerFieldsDto;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter;
+
+/**
+ * Rejects creating an owner whose supplied registrationDate is later than the server's current
+ * date, answering 400. Kept as its own advice so this rule stays a small, self-contained unit
+ * rather than growing the controller or another handler.
+ */
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class OwnerFutureRegistrationDateAdvice extends RequestBodyAdviceAdapter {
+
+    @Override
+    public boolean supports(MethodParameter methodParameter, Type targetType,
+                            Class<? extends HttpMessageConverter<?>> converterType) {
+        return OwnerFieldsDto.class.equals(targetType);
+    }
+
+    @Override
+    public Object afterBodyRead(Object body, HttpInputMessage inputMessage, MethodParameter parameter,
+                                Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+        LocalDate supplied = ((OwnerFieldsDto) body).getRegistrationDate();
+        if (supplied != null && supplied.isAfter(LocalDate.now())) {
+            throw new FutureRegistrationDateException();
+        }
+        return body;
+    }
+
+    @ExceptionHandler(FutureRegistrationDateException.class)
+    @ResponseBody
+    public ResponseEntity<String> handleFutureRegistrationDate(FutureRegistrationDateException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    /** Signals that the supplied registration date lies in the future. */
+    static class FutureRegistrationDateException extends RuntimeException {
+
+        FutureRegistrationDateException() {
+            super("registration date must not be later than the current date");
+        }
+    }
+}
