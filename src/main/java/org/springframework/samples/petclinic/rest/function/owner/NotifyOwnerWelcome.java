49b9ee7fdc3d3826eb6df4a68d09b44c0d85f94e
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-01 11:23:56 +0800

    cp57 agent welcome-notify

---
+++ src/main/java/org/springframework/samples/petclinic/rest/function/owner/NotifyOwnerWelcome.java
@@ -0,0 +1,21 @@
+package org.springframework.samples.petclinic.rest.function.owner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.officefloor.plugin.variable.Val;
+import org.springframework.samples.petclinic.model.Owner;
+
+/**
+ * Enqueues a welcome notification once a create has succeeded by emitting a line to the
+ * dedicated {@code NOTIFY} logger. Runs after {@code SaveOwner} so the persisted owner's id
+ * is available alongside the assigned memberId.
+ */
+public class NotifyOwnerWelcome {
+
+    private static final Logger NOTIFY = LoggerFactory.getLogger("NOTIFY");
+
+    public void service(@Val Owner owner) {
+        NOTIFY.info("welcome owner id={} memberId={}", owner.getId(), owner.getMemberId());
+    }
+}
