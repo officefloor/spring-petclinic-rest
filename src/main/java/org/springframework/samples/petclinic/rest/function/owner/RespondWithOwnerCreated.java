7909147f7a3227e80a6b78970c6724952bc19da5
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-03 05:58:05 +0800

    cp22 agent bulk-warning

--- src/main/java/org/springframework/samples/petclinic/rest/function/owner/RespondWithOwnerCreated.java
+++ src/main/java/org/springframework/samples/petclinic/rest/function/owner/RespondWithOwnerCreated.java
@@ -1,21 +1,27 @@
 package org.springframework.samples.petclinic.rest.function.owner;
 
 import java.net.URI;
+import java.time.LocalDate;
 
 import net.officefloor.plugin.variable.Val;
 import net.officefloor.web.ObjectResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.samples.petclinic.mapper.OwnerMapper;
 import org.springframework.samples.petclinic.model.Owner;
+import org.springframework.samples.petclinic.repository.OwnerRepository;
 import org.springframework.samples.petclinic.rest.dto.OwnerDto;
 
 public class RespondWithOwnerCreated {
 
     private static final org.slf4j.Logger AUDIT = org.slf4j.LoggerFactory.getLogger("AUDIT");
 
-    public void service(@Val Owner owner, OwnerMapper ownerMapper,
+    public void service(@Val Owner owner, OwnerMapper ownerMapper, OwnerRepository ownerRepository,
             ObjectResponse<ResponseEntity<OwnerDto>> response) {
         OwnerDto dto = ownerMapper.toOwnerDto(owner);
+        LocalDate today = BusinessDay.roll(LocalDate.now());
+        long createdToday = ownerRepository.findAll().stream()
+                .filter(other -> today.equals(other.getRegistrationDate())).count();
+        dto.setBulkSignupWarning(createdToday > 80);
         AUDIT.info("owner created id={} customerCode={} registrationDate={}",
                 owner.getId(), owner.getCustomerCode(), owner.getRegistrationDate());
         response.send(ResponseEntity.created(URI.create("/api/owners/" + owner.getId())).body(dto));
