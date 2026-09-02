39ce4678e5a7fd286d41023077011d20e829c3ed
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-02 08:51:28 +0800

    cp42 agent locality-timezone

--- src/main/java/org/springframework/samples/petclinic/rest/function/owner/RespondWithOwner.java
+++ src/main/java/org/springframework/samples/petclinic/rest/function/owner/RespondWithOwner.java
@@ -1,5 +1,7 @@
 package org.springframework.samples.petclinic.rest.function.owner;
 
+import java.util.Map;
+
 import net.officefloor.plugin.variable.Val;
 import net.officefloor.web.ObjectResponse;
 import org.springframework.samples.petclinic.mapper.OwnerMapper;
@@ -8,6 +10,9 @@ import org.springframework.samples.petclinic.repository.OwnerRepository;
 import org.springframework.samples.petclinic.rest.dto.OwnerDto;
 
 public class RespondWithOwner {
+
+    private static final Map<String, String> REGION_TIMEZONE = Map.of(
+            "NSW", "Australia/Sydney", "VIC", "Australia/Melbourne", "QLD", "Australia/Brisbane");
 
     public void service(@Val Owner owner, OwnerMapper ownerMapper, OwnerRepository ownerRepository,
             ObjectResponse<OwnerDto> response) {
@@ -18,6 +23,7 @@ public class RespondWithOwner {
         dto.setMembershipPoints(membershipPoints);
         dto.setMembershipLevel(MembershipLevel.of(membershipPoints));
         dto.setLocality(Locality.of(owner));
+        dto.setTimezone(REGION_TIMEZONE.get(dto.getLocality()));
         dto.setBulkSignupWarning(BulkSignupWarning.of(owner, ownerRepository));
         dto.setContactPreference(ContactPreference.of(owner));
         Integer possibleDuplicateOf = PossibleDuplicate.of(owner, ownerRepository);
