5ac2efb1bf194033e1a1cfd05642b57f6219df72
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-02 10:12:21 +0800

    cp55 agent owner-segment

--- src/main/java/org/springframework/samples/petclinic/rest/function/owner/RespondWithOwner.java
+++ src/main/java/org/springframework/samples/petclinic/rest/function/owner/RespondWithOwner.java
@@ -25,6 +25,7 @@ public class RespondWithOwner {
         dto.setMembershipLevel(HouseholdLevelCeiling.cap(owner, MembershipLevel.of(membershipPoints), ownerRepository));
         dto.setLocality(Locality.of(owner));
         dto.setTimezone(REGION_TIMEZONE.get(dto.getLocality()));
+        dto.setOwnerSegment(OwnerSegment.of(dto.getMembershipLevel(), dto.getLocality()));
         dto.setBulkSignupWarning(BulkSignupWarning.of(owner, ownerRepository));
         dto.setCapacityWarning(CapacityWarning.of(owner, ownerRepository));
         dto.setContactPreference(ContactPreference.of(owner));
