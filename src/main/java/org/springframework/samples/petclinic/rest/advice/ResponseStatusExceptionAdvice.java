7f255fb90c5720a7cd6adfb3bb6ffeb365915dcd
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-02 11:04:51 +0800

    cp03 agent telephone-unique

---
+++ src/main/java/org/springframework/samples/petclinic/rest/advice/ResponseStatusExceptionAdvice.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.samples.petclinic.rest.advice;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+/**
+ * Maps a {@link ResponseStatusException} to its carried HTTP status. Declared with the highest
+ * precedence so it is consulted before the catch-all {@link ExceptionControllerAdvice}, which would
+ * otherwise report it as a 500.
+ */
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice
+public class ResponseStatusExceptionAdvice {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Void> handleResponseStatusException(ResponseStatusException e) {
+        return ResponseEntity.status(e.getStatusCode()).build();
+    }
+}
