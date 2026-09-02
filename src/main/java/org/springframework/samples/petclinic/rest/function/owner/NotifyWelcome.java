baf0ffa0093a891c975b0ee6e6950098c7448341
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-02 21:50:22 +0800

    cp57 agent welcome-notify

---
+++ src/main/java/org/springframework/samples/petclinic/rest/function/owner/NotifyWelcome.java
@@ -0,0 +1,16 @@
+package org.springframework.samples.petclinic.rest.function.owner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.officefloor.plugin.variable.Val;
+import org.springframework.samples.petclinic.model.Owner;
+
+public class NotifyWelcome {
+
+    private static final Logger NOTIFY = LoggerFactory.getLogger("NOTIFY");
+
+    public void service(@Val Owner owner) {
+        NOTIFY.info("welcome owner id={} memberId={}", owner.getId(), owner.getCustomerCode());
+    }
+}
