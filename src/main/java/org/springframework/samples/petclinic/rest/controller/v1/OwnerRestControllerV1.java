71a7b6db5bc8b0f722000bbdf0843a7abed6d1ef
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-02 11:39:19 +0800

    cp09 agent customer-code

--- src/main/java/org/springframework/samples/petclinic/rest/controller/v1/OwnerRestControllerV1.java
+++ src/main/java/org/springframework/samples/petclinic/rest/controller/v1/OwnerRestControllerV1.java
@@ -101,6 +101,8 @@ public class OwnerRestControllerV1 implements OwnersApi {
     public ResponseEntity<OwnerDto> addOwner(OwnerFieldsDto ownerFieldsDto) {
         HttpHeaders headers = new HttpHeaders();
         Owner owner = ownerMapper.toOwner(ownerFieldsDto);
+        String last3 = owner.getLastName().substring(0, Math.min(3, owner.getLastName().length())).toUpperCase();
+        owner.setCustomerCode(String.format("%s-%04d", last3, this.clinicService.findAllOwners().size() + 1));
         this.clinicService.saveOwner(owner);
         OwnerDto ownerDto = ownerMapper.toOwnerDto(owner);
         headers.setLocation(UriComponentsBuilder.newInstance()
