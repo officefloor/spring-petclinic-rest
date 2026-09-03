2fe7ff45276c7a38c3b9dbcf9a7c6e06620a5cbd
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-03 19:19:13 +0800

    cp47 agent holiday-business-day

--- src/main/java/org/springframework/samples/petclinic/rest/function/owner/AdjustRegistrationDate.java
+++ src/main/java/org/springframework/samples/petclinic/rest/function/owner/AdjustRegistrationDate.java
@@ -3,6 +3,7 @@ package org.springframework.samples.petclinic.rest.function.owner;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 import net.officefloor.plugin.variable.Val;
 import org.springframework.samples.petclinic.model.Owner;
@@ -15,12 +16,18 @@ import org.springframework.samples.petclinic.rest.escalation.MissingOwnerFieldsE
  */
 public class AdjustRegistrationDate {
 
+    private static final Set<LocalDate> HOLIDAYS = Set.of(
+            LocalDate.parse("2026-01-01"), LocalDate.parse("2026-01-26"),
+            LocalDate.parse("2026-04-25"), LocalDate.parse("2026-12-25"),
+            LocalDate.parse("2026-12-28"));
+
     public void service(@Val Owner owner) throws MissingOwnerFieldsException {
         LocalDate date = owner.getRegistrationDate();
         if (date.isAfter(LocalDate.now())) {
             throw new MissingOwnerFieldsException(List.of("registrationDate"));
         }
-        while (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY) {
+        while (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY
+                || HOLIDAYS.contains(date)) {
             date = date.plusDays(1);
         }
         owner.setRegistrationDate(date);
