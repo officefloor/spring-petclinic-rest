1e5c5380f80a12ebeda78b382e36be3a44e6db87
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-03 22:57:54 +0800

    cp22 agent bulk-warning

---
+++ src/main/java/org/springframework/samples/petclinic/rest/advice/OwnerBulkSignupAdvice.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2016-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.samples.petclinic.rest.advice;
+
+import java.time.LocalDate;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.samples.petclinic.model.Owner;
+import org.springframework.samples.petclinic.rest.dto.OwnerDto;
+import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.samples.petclinic.util.BusinessDays;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * Flags every owner response with {@code bulkSignupWarning}: true once more than 80 owners
+ * have already been created today (by registrationDate), otherwise false. Purely informational,
+ * so unlike the daily-limit rule it never rejects a request.
+ */
+@ControllerAdvice
+public class OwnerBulkSignupAdvice implements ResponseBodyAdvice<Object> {
+
+    private static final int BULK_SIGNUP_THRESHOLD = 80;
+
+    private final ClinicService clinicService;
+
+    public OwnerBulkSignupAdvice(ClinicService clinicService) {
+        this.clinicService = clinicService;
+    }
+
+    @Override
+    public boolean supports(MethodParameter returnType,
+                            Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request, ServerHttpResponse response) {
+        if (body instanceof OwnerDto owner) {
+            owner.setBulkSignupWarning(createdTodayCount() > BULK_SIGNUP_THRESHOLD);
+        }
+        return body;
+    }
+
+    private int createdTodayCount() {
+        LocalDate today = BusinessDays.toBusinessDay(LocalDate.now());
+        int count = 0;
+        for (Owner existing : clinicService.findAllOwners()) {
+            if (today.equals(existing.getRegistrationDate())) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
