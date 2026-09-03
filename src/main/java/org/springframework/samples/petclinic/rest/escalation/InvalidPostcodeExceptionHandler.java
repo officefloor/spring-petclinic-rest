4d44b5517f93d230b13b31d2707efb0adebb67d0
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-03 09:31:45 +0800

    cp59 agent problem-json

--- src/main/java/org/springframework/samples/petclinic/rest/escalation/InvalidPostcodeExceptionHandler.java
+++ src/main/java/org/springframework/samples/petclinic/rest/escalation/InvalidPostcodeExceptionHandler.java
@@ -3,12 +3,14 @@ package org.springframework.samples.petclinic.rest.escalation;
 import net.officefloor.plugin.section.clazz.Parameter;
 import net.officefloor.web.ObjectResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 
 public class InvalidPostcodeExceptionHandler {
 
     public void handle(@Parameter InvalidPostcodeException ex,
-            ObjectResponse<ResponseEntity<String>> response) {
-        response.send(new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST));
+            ObjectResponse<ResponseEntity<ProblemDetail>> response) {
+        response.send(ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ProblemDetails.build(ex, HttpStatus.BAD_REQUEST, ex.getMessage())));
     }
 }
