4f31b8c98f3304d06e1a8f34d7b0a58d35748160
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-04 00:32:16 +0800

    cp38 agent email-blocklist

---
+++ src/main/java/org/springframework/samples/petclinic/rest/advice/OwnerDisposableEmailAdvice.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2016-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.samples.petclinic.rest.advice;
+
+import java.lang.reflect.Type;
+import java.util.Locale;
+import java.util.Set;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.samples.petclinic.rest.dto.OwnerFieldsDto;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdvice;
+
+/**
+ * Rejects a create/update owner request whose email address uses a disposable domain
+ * (mailinator.com, tempmail.com, guerrillamail.com), responding 400 Bad Request. The
+ * domain is matched case-insensitively; an absent email is left untouched.
+ */
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class OwnerDisposableEmailAdvice implements RequestBodyAdvice {
+
+    private static final Set<String> DISPOSABLE_DOMAINS =
+        Set.of("mailinator.com", "tempmail.com", "guerrillamail.com");
+
+    @Override
+    public boolean supports(MethodParameter methodParameter, Type targetType,
+                            Class<? extends HttpMessageConverter<?>> converterType) {
+        return OwnerFieldsDto.class.equals(targetType);
+    }
+
+    @Override
+    public Object afterBodyRead(Object body, HttpInputMessage inputMessage, MethodParameter parameter,
+                                Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+        if (isDisposable(((OwnerFieldsDto) body).getEmail())) {
+            throw new DisposableEmailException();
+        }
+        return body;
+    }
+
+    private boolean isDisposable(String email) {
+        if (email == null) {
+            return false;
+        }
+        int at = email.lastIndexOf('@');
+        return at >= 0 && DISPOSABLE_DOMAINS.contains(email.substring(at + 1).toLowerCase(Locale.ROOT));
+    }
+
+    @Override
+    public Object handleEmptyBody(Object body, HttpInputMessage inputMessage, MethodParameter parameter,
+                                  Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return body;
+    }
+
+    @Override
+    public HttpInputMessage beforeBodyRead(HttpInputMessage inputMessage, MethodParameter parameter,
+                                           Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return inputMessage;
+    }
+
+    @ExceptionHandler(DisposableEmailException.class)
+    @ResponseBody
+    public ResponseEntity<Void> handleDisposableEmail(DisposableEmailException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    /** Signals that the email address uses a disposable domain. */
+    static class DisposableEmailException extends RuntimeException {
+    }
+}
