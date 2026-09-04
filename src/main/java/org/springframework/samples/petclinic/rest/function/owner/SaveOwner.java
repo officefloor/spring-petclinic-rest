ab4c129da9e423a44ff86a0f29eb237a760ae4d5
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-04 17:31:01 +0800

    cp43 agent audit-enriched

--- src/main/java/org/springframework/samples/petclinic/rest/function/owner/SaveOwner.java
+++ src/main/java/org/springframework/samples/petclinic/rest/function/owner/SaveOwner.java
@@ -3,6 +3,9 @@ package org.springframework.samples.petclinic.rest.function.owner;
 import net.officefloor.plugin.variable.Val;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.samples.petclinic.mapper.MembershipLevel;
+import org.springframework.samples.petclinic.mapper.MembershipNumber;
+import org.springframework.samples.petclinic.mapper.MembershipPoints;
 import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.repository.OwnerRepository;
 
@@ -12,7 +15,11 @@ public class SaveOwner {
 
     public void service(@Val Owner owner, OwnerRepository ownerRepository) {
         ownerRepository.save(owner);
-        audit.info("owner created id={} customerCode={} registrationDate={}",
-                owner.getId(), owner.getCustomerCode(), owner.getRegistrationDate());
+        String membershipNumber = MembershipNumber.of(owner.getCustomerCode(), owner.getRegistrationDate());
+        int membershipLevel = MembershipLevel.of(MembershipPoints.of(owner.getNamesakeCount(),
+                owner.getEmail(), owner.getRegistrationDate(), HouseholdTier.isGold(owner, ownerRepository)));
+        audit.info("owner created id={} customerCode={} registrationDate={} membershipLevel={} membershipNumber={}",
+                owner.getId(), owner.getCustomerCode(), owner.getRegistrationDate(),
+                membershipLevel, membershipNumber);
     }
 }
