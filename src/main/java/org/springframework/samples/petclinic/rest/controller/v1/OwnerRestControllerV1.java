2af3dba930914620ff327ea34b937372434a9a88
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-04 14:23:03 +0800

    cp43 agent audit-enriched

--- src/main/java/org/springframework/samples/petclinic/rest/controller/v1/OwnerRestControllerV1.java
+++ src/main/java/org/springframework/samples/petclinic/rest/controller/v1/OwnerRestControllerV1.java
@@ -165,9 +165,10 @@ public class OwnerRestControllerV1 implements OwnersApi {
         owner.setHouseholdSize(countHouseholdMembers(owner.getHouseholdId()) + 1);
         markPossibleDuplicate(owner, sharesHousehold);
         this.clinicService.saveOwner(owner);
-        AUDIT.info("owner created id={} customerCode={} registrationDate={}",
-            owner.getId(), owner.getCustomerCode(), owner.getRegistrationDate());
         OwnerDto ownerDto = ownerMapper.toOwnerDto(owner);
+        AUDIT.info("owner created id={} customerCode={} registrationDate={} membershipLevel={} membershipNumber={}",
+            owner.getId(), owner.getCustomerCode(), owner.getRegistrationDate(),
+            ownerDto.getMembershipLevel(), ownerDto.getMembershipNumber());
         headers.setLocation(UriComponentsBuilder.newInstance()
             .path("/api/owners/{id}").buildAndExpand(owner.getId()).toUri());
         return new ResponseEntity<>(ownerDto, headers, HttpStatus.CREATED);
