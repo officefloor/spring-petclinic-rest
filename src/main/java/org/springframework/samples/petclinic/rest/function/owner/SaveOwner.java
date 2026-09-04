09794f8704752193babc96726c1bcdfbe8de61a5
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-04 15:18:47 +0800

    cp21 agent audit-create

--- src/main/java/org/springframework/samples/petclinic/rest/function/owner/SaveOwner.java
+++ src/main/java/org/springframework/samples/petclinic/rest/function/owner/SaveOwner.java
@@ -1,12 +1,18 @@
 package org.springframework.samples.petclinic.rest.function.owner;
 
 import net.officefloor.plugin.variable.Val;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.repository.OwnerRepository;
 
 public class SaveOwner {
 
+    private static final Logger audit = LoggerFactory.getLogger("AUDIT");
+
     public void service(@Val Owner owner, OwnerRepository ownerRepository) {
         ownerRepository.save(owner);
+        audit.info("owner created id={} customerCode={} registrationDate={}",
+                owner.getId(), owner.getCustomerCode(), owner.getRegistrationDate());
     }
 }
