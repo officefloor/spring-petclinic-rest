f8c33aba91966e078420afb5b11c02675e36fd99
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-05 03:21:39 +0800

    cp21 agent audit-create

---
+++ src/main/java/org/springframework/samples/petclinic/rest/function/owner/LogAuditOnCreate.java
@@ -0,0 +1,21 @@
+package org.springframework.samples.petclinic.rest.function.owner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.officefloor.plugin.variable.Val;
+import org.springframework.samples.petclinic.model.Owner;
+
+/**
+ * Emits an audit line via the dedicated {@code AUDIT} logger once an owner has been
+ * created, carrying the owner id, customerCode and registrationDate.
+ */
+public class LogAuditOnCreate {
+
+    private static final Logger AUDIT = LoggerFactory.getLogger("AUDIT");
+
+    public void service(@Val Owner owner) {
+        AUDIT.info("owner created id={} customerCode={} registrationDate={}",
+            owner.getId(), owner.getCustomerCode(), owner.getRegistrationDate());
+    }
+}
