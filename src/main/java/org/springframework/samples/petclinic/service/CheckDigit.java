dc4afdb94ad3bcc7a1efeb1adfec53387e3f4a2c
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-05 10:10:08 +0800

    cp31 agent check-digit

---
+++ src/main/java/org/springframework/samples/petclinic/service/CheckDigit.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.samples.petclinic.service;
+
+/**
+ * Computes the Luhn check digit (0-9) over the decimal digits contained in a customerCode,
+ * ignoring any non-digit characters.
+ */
+public final class CheckDigit {
+
+    private CheckDigit() {
+    }
+
+    public static int of(String customerCode) {
+        int sum = 0;
+        boolean dbl = true;
+        for (int i = customerCode.length() - 1; i >= 0; i--) {
+            char c = customerCode.charAt(i);
+            if (c < '0' || c > '9') {
+                continue;
+            }
+            int d = c - '0';
+            if (dbl) {
+                d *= 2;
+                if (d > 9) {
+                    d -= 9;
+                }
+            }
+            sum += d;
+            dbl = !dbl;
+        }
+        return (10 - (sum % 10)) % 10;
+    }
+}
