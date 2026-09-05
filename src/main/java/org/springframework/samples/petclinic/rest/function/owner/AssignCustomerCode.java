371e9f5ea359e6b22ee790ad1e1b5f040604d18f
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-05 14:41:21 +0800

    cp16 agent customer-code-city

--- src/main/java/org/springframework/samples/petclinic/rest/function/owner/AssignCustomerCode.java
+++ src/main/java/org/springframework/samples/petclinic/rest/function/owner/AssignCustomerCode.java
@@ -7,9 +7,19 @@ import org.springframework.samples.petclinic.repository.OwnerRepository;
 public class AssignCustomerCode {
 
     public void service(@Val Owner owner, OwnerRepository ownerRepository) {
-        String lastName = owner.getLastName();
-        String last3 = lastName.substring(0, Math.min(3, lastName.length())).toUpperCase();
-        int sequence = ownerRepository.findAll().size() + 1;
-        owner.setCustomerCode(String.format("%s-%04d", last3, sequence));
+        String city3 = prefix3(owner.getCity());
+        String last3 = prefix3(owner.getLastName());
+        int sequence = countInCity(ownerRepository, owner.getCity()) + 1;
+        owner.setCustomerCode(String.format("%s-%s-%04d", city3, last3, sequence));
+    }
+
+    private static String prefix3(String value) {
+        return value.substring(0, Math.min(3, value.length())).toUpperCase();
+    }
+
+    private static int countInCity(OwnerRepository ownerRepository, String city) {
+        return (int) ownerRepository.findAll().stream()
+            .filter(other -> city.equals(other.getCity()))
+            .count();
     }
 }
