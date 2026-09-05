9e1016ea67d4e3ec182f8590001f119009025b7d
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-05 18:52:21 +0800

    cp59 agent problem-json

--- src/main/java/org/springframework/samples/petclinic/rest/escalation/CityAtCapacityExceptionHandler.java
+++ src/main/java/org/springframework/samples/petclinic/rest/escalation/CityAtCapacityExceptionHandler.java
@@ -3,12 +3,14 @@ package org.springframework.samples.petclinic.rest.escalation;
 import net.officefloor.plugin.section.clazz.Parameter;
 import net.officefloor.web.ObjectResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 
 public class CityAtCapacityExceptionHandler {
 
     public void handle(@Parameter CityAtCapacityException ex,
-            ObjectResponse<ResponseEntity<Void>> response) {
-        response.send(ResponseEntity.status(HttpStatus.CONFLICT).build());
+            ObjectResponse<ResponseEntity<ProblemDetail>> response) {
+        ProblemDetail detail = ProblemDetails.build(ex, HttpStatus.CONFLICT, ex.getMessage());
+        response.send(ResponseEntity.status(HttpStatus.CONFLICT).body(detail));
     }
 }
--- src/main/java/org/springframework/samples/petclinic/rest/escalation/DailyLimitExceptionHandler.java
+++ src/main/java/org/springframework/samples/petclinic/rest/escalation/DailyLimitExceptionHandler.java
@@ -3,12 +3,14 @@ package org.springframework.samples.petclinic.rest.escalation;
 import net.officefloor.plugin.section.clazz.Parameter;
 import net.officefloor.web.ObjectResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 
 public class DailyLimitExceptionHandler {
 
     public void handle(@Parameter DailyLimitException ex,
-            ObjectResponse<ResponseEntity<Void>> response) {
-        response.send(ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build());
+            ObjectResponse<ResponseEntity<ProblemDetail>> response) {
+        ProblemDetail detail = ProblemDetails.build(ex, HttpStatus.TOO_MANY_REQUESTS, ex.getMessage());
+        response.send(ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body(detail));
     }
 }
