496c28c167007bb725c2e4b2c8097f16723c22a6
Daniel Sagenschneider <daniel@officefloor.net>
2026-09-11 18:49:47 +0800

    cp59 agent problem-json

--- src/main/java/org/springframework/samples/petclinic/rest/escalation/RequiredFieldsExceptionHandler.java
+++ src/main/java/org/springframework/samples/petclinic/rest/escalation/RequiredFieldsExceptionHandler.java
@@ -3,13 +3,15 @@ package org.springframework.samples.petclinic.rest.escalation;
 import net.officefloor.plugin.section.clazz.Parameter;
 import net.officefloor.web.ObjectResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 
 public class RequiredFieldsExceptionHandler {
 
     public void handle(@Parameter RequiredFieldsException ex,
-            ObjectResponse<ResponseEntity<ValidationErrorsResponse>> response) {
-        response.send(ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ValidationErrorsResponse(ex.getErrors())));
+            ObjectResponse<ResponseEntity<ProblemDetail>> response) {
+        ProblemDetail detail = ProblemDetails.build(ex, HttpStatus.BAD_REQUEST, ex.getMessage());
+        detail.setProperty("errors", ex.getErrors());
+        response.send(ResponseEntity.status(HttpStatus.BAD_REQUEST).body(detail));
     }
 }
